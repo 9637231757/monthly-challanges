7fa38b60fd1a1247c3ad1722ecfcd2e0544b018d
9637231757 <er.rohangaidhani@gmail.com>
2025-03-11 00:31:16 +0530

    applied html

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        
+      
+        path("", views.index),        
         path('<int:month>/', views.monthly_challenges_dr, name = 'months'),    
         path('<str:month>/', views.monthly_challenges, name = 'month_challenge'),  
   
--- challenges/views.py
+++ challenges/views.py
@@ -3,19 +3,33 @@ from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 from django.urls import reverse
 
 monthly_challenges_dict = {
-    "january":"Be vegeterial for a month",
-    "feburary":"learn django for 20 minutes everyday",
-    "march":"do excercies for 1 hour everyday",
-    "april":"Be vegeterial for a month",
+    "january":"Be vegetarian for a month",
+    "february":"learn django for 20 minutes everyday",
+    "march":"do excercises for 1 hour everyday",
+    "april":"Be vegetarian for a month",
     "may":"learn django for 20 minutes everyday",
-    "june":"do excercies for 1 hour everyday",
-    "july":"Be vegeterial for a month",
+    "june":"do excercises for 1 hour everyday",
+    "july":"Be vegetarian for a month",
     "august":"learn django for 20 minutes everyday",
-    "september":"do excercies for 1 hour everyday",
-    "november":"Be vegeterial for a month",
-    "december":"do excercies for 1 hour everyday"
+    "september":"do excercises for 1 hour everyday",
+    "november":"Be vegetarian for a month",
+    "december":"do excercises for 1 hour everyday"
 }
 # Create your views here.
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges_dict.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse('month_challenge', args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+        
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+        
+
 
 def monthly_challenges_dr(request, month):
     months = list(monthly_challenges_dict.keys())
