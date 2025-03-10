615d0f699e9f25c8804327eec992e4aa967287c1
9637231757 <er.rohangaidhani@gmail.com>
2025-03-10 18:00:41 +0530

    created app

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
+
+def monthly_challenges(request, month):
+    nexttext = None
+    if month == 'january':
+        nextext = 'Be vegeterial for a month'
+    elif month == 'february':
+        nextext ='learn django for minutes everyday '
+    elif month == 'march':
+        nextext = 'excercise everyday for 1 hour '
+    else:
+        return HttpResponseNotFound('This month does not exit:') 
+    return HttpResponse(nextext)           
+                    
