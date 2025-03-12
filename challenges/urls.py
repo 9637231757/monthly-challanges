336608a81578a6f39fcbee74ae28e536b97cca8e
9637231757 <er.rohangaidhani@gmail.com>
2025-03-12 18:00:53 +0530

    assigned url for index for snippets

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
       
-        path("", views.index), #/challenges/       
+        path("", views.index, name= 'index'), #/challenges/       
         path('<int:month>/', views.monthly_challenges_dr),    
         path('<str:month>/', views.monthly_challenges, name='month-challenge'),
 
